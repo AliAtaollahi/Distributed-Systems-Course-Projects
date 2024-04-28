e908de550129c51850a423fea9dc8c17dc14124d
mohajel <msadegi007@yahoo.com>
2024-04-28 13:56:17 +0330

    feat: show events

--- CA2/cmd/ticketServices.go
+++ CA2/cmd/ticketServices.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 type TicketServices struct {
 	cache Cache
 }
 
 func (ts *TicketServices) showEvents() {
+
+
 	// lock it
 	// for in cash
 	// log it
@@ -12,8 +20,19 @@ func (ts *TicketServices) showEvents() {
 }
 
 func (ts *TicketServices) buyTicket(eventID int, numberOfTickets int) {
+
+	logFileName := fmt.Sprintf("./log/ticketHandler.txt")
+	file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	logger := log.New(file, fmt.Sprintf("event >> "), log.LstdFlags)
+
+
 	// buy it
 	// log it
+	logger.Println("ticket sb bout .....")
 	// print it on consol
 	// make sure to lock when using cash part then unlock
 }
